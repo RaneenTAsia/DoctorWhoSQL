b83501713950a5645f2c96cba723ffd70c3e49ac
Raneen Asia <Raneenasia101@gmail.com>
2023-03-11 20:28:38 +0200

    Add Task 3 File

--- DoctorWhoSQL/DoctorWhoSQL/DoctorWhoTablesCreateQueries.sql
+++ DoctorWhoSQL/DoctorWhoSQL/DoctorWhoTablesCreateQueries.sql
@@ -3,58 +3,58 @@ GO
 
 CREATE TABLE tblEnemy
 (
-	EnemyId INT NOT NULL PRIMARY KEY,
+	EnemyId INT NOT NULL PRIMARY KEY IDENTITY,
 	EnemyName VARCHAR(30),
 	Description VARCHAR(255)
 )
 
 CREATE TABLE tblEpisodeEnemy
 (
-	EpisodeEnemyId INT NOT NULL PRIMARY KEY,
+	EpisodeEnemyId INT NOT NULL PRIMARY KEY IDENTITY,
 	EpisodeId INT NOT NULL,
 	EnemyId INT NOT NULL
 )
 
 CREATE TABLE tblEpisode
 (
-	EpisodeId INT NOT NULL PRIMARY KEY,
+	EpisodeId INT NOT NULL PRIMARY KEY IDENTITY,
 	SeriesNumber INT,
 	EpisodeNumber INT,
 	EpisodeType VARCHAR(30),
 	Title VARCHAR(255),
-	EpisodeDate DATETIME,
-	AuthorID INT NOT NULL,
+	EpisodeDate DATE,
+	AuthorId INT NOT NULL,
 	DoctorId INT NOT NULL,
 	Notes VARCHAR(255)
 )
 
 CREATE TABLE tblEpisodeCompanion
 (
-	EpisodeCompanionId INT NOT NULL PRIMARY KEY,
+	EpisodeCompanionId INT NOT NULL PRIMARY KEY IDENTITY,
 	EpisodeId INT NOT NULL,
 	CompanionId INT NOT NULL 
 )
 
 CREATE TABLE tblCompanion
 ( 
-	CompanionID INT NOT NULL PRIMARY KEY,
+	CompanionID INT NOT NULL PRIMARY KEY IDENTITY,
 	CompanionName VARCHAR(30),
 	WhoPlayed VARCHAR(30)
 )
 
 CREATE TABLE tblDoctor
 (
-	DoctorId INT NOT NULL PRIMARY KEY,
+	DoctorId INT NOT NULL PRIMARY KEY IDENTITY,
 	DoctorNumber INT,
 	DoctorName VARCHAR(30),
-	BirthDate DATETIME,
-	FirstEpisodeDate DATETIME,
-	LastEpisodeDate DATETIME
+	BirthDate DATE,
+	FirstEpisodeDate DATE,
+	LastEpisodeDate DATE
 )
 
 CREATE TABLE tblAuthor
 (
-	AuthorId INT NOT NULL PRIMARY KEY,
+	AuthorId INT NOT NULL PRIMARY KEY IDENTITY,
 	AuthorName VARCHAR(30)
 )
 
@@ -79,3 +79,4 @@ ALTER TABLE tblEpisodeCompanion
 ADD FOREIGN KEY (CompanionId) REFERENCES tblCompanion(CompanionId);
 
 GO
+
